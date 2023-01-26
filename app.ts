e99c4dad4d43ba69277c8a09db93179907d95fe2
madrodgerflynn <madrodgerflynn@gmail.com>
2023-01-25 18:38:21 -0700

    practicing

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean) {
+    console.log(typeof number1)
+    if (showResult) {
+        console.log(n1+n2) 
+    } else {
+        return n1 + n2;
+    }
     return n1 +n2;
 }
 const number1=5;
 const number2= 2.8;
+const printResult= true;
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult);
 console.log(result);
