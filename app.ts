91dac18a210a585eb921b72aa709de4cf8c0b703
madrodgerflynn <madrodgerflynn@gmail.com>
2023-01-25 19:04:43 -0700

    practicing

--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-const result= n1+n2;
-    if (showResult) {
-        console.log(phrase + result) 
-    } else {
-        return n1 + n2;
-    }
-    return n1 +n2;
-}
-const number1=5;
-const number2= 2.8;
-const printResult= true;
-const resultPhrase= "Result is: ";
+const person: {
+    name: string;
+    age: number;
+}= {
+    name: 'Tyler',
+    age: 35
+};
 
- add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
