8be4d504c0b1986d5e5043123ae1ba945bfc3c9b
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-05 13:42:06 -0700

    TS practice anytype

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
-var ROLE;
-(function (ROLE) {
-    ROLE[ROLE["ADMIN"] = 0] = "ADMIN";
-    ROLE[ROLE["READ_ONLY"] = 1] = "READ_ONLY";
-    ROLE[ROLE["AUTHOR"] = 2] = "AUTHOR";
-})(ROLE || (ROLE = {}));
-;
-// enum assigns labels to numbers
-var person = {
-    name: 'Tyler',
-    age: 35,
-    hobbies: ['Sports', 'Cooking'],
-    role: ROLE.ADMIN
-    // tupal - fixed length arrag: must have a numerica identifiey with a string identifier human readable
-};
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toLocaleUpperCase());
+function combine(input1, input2) {
+    // union types that allow mulitiple params
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === ROLE.ADMIN) {
-    console.log('is ADMIN');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combineNames = combine('Tyler', 'Tae');
+console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-function combine(input1: number | string , input2: number | string) {
+function combine(input1: number | string , input2: number | string, resultConversion: string) {
     // union types that allow mulitiple params
     let result;
     if (typeof input1=== 'number' && typeof input2 === 'number') {
@@ -7,11 +7,19 @@ function combine(input1: number | string , input2: number | string) {
     }else {
         result= input1.toString() + input2.toString();
     }
-      return result;
+    if(resultConversion === 'as-number'){
+        return +result;
+    }else {
+        return result.toString();
     }
-    const combinedAges= combine(30, 26);
+    }
+    const combinedAges= combine(30, 26, 'as-number');
     console.log(combinedAges);
 
-    const combineNames = combine('Tyler', 'Tae');
+    const combineStringAges = combine('30', '26', 'as-number')
+    console.log(combineStringAges);
+    
+
+    const combineNames = combine('Tyler', 'Tae', 'as-text');
     console.log(combineNames);
     
