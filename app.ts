b6a034fc9d2db9ef8fa35ade286ebce5df2930e7
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-05 13:25:32 -0700

    TS practice anytype

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-function combine(n1: number, n2: number) {
-    const result= n1+n2;
+function combine(input1: number, input2: number) {
+    const result= input1+input2;
       return result;
     }
+    const combinedAges= combine(30, 26);
