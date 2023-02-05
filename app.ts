96558b5bd2533f9bd942e0ae404757740b6eaa02
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-05 13:34:06 -0700

    TS practice anytype

--- app.ts
+++ app.ts
@@ -1,5 +1,17 @@
-function combine(input1: number, input2: number) {
-    const result= input1+input2;
+function combine(input1: number | string , input2: number | string) {
+    // union types that allow mulitiple params
+    let result;
+    if (typeof input1=== 'number' && typeof input2 === 'number') {
+
+        result= input1+input2;
+    }else {
+        result= input1.toString() + input2.toString();
+    }
       return result;
     }
     const combinedAges= combine(30, 26);
+    console.log(combinedAges);
+
+    const combineNames = combine('Tyler', 'Tae');
+    console.log(combineNames);
+    
