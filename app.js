f02f2c6bd0bb96455e124b0282df762f59516ee0
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-05 18:37:33 -0700

    TS practice anytype

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     // union types that allow mulitiple params
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+        // adding + will convert into a number
     }
     else {
         result = input1.toString() + input2.toString();
     }
+    // if(resultConversion === 'as-number'){
+    //     return +result;
+    // }else {
+    //     return result.toString();
+    // }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combineNames = combine('Tyler', 'Tae');
+var combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+var combineNames = combine('Tyler', 'Tae', 'as-text');
 console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,17 +1,22 @@
-function combine(input1: number | string , input2: number | string, resultConversion: string) {
-    // union types that allow mulitiple params
-    let result;
-    if (typeof input1=== 'number' && typeof input2 === 'number') {
+type Combinable = number;
 
-        result= input1+input2;
+function combine(input1: number | string , input2: number | string, resultConversion: 'as-number'|'as-text') {
+    // union types that allow mulitiple params
+    // used in conjuntion with literal types = 'as-number' & 'as-text'
+    let result;
+    if (typeof input1=== 'number' && typeof input2 === 'number'|| resultConversion === 'as-number') {
+
+        result= +input1+ +input2;
+        // adding + will convert into a number
     }else {
         result= input1.toString() + input2.toString();
     }
-    if(resultConversion === 'as-number'){
-        return +result;
-    }else {
-        return result.toString();
-    }
+    // if(resultConversion === 'as-number'){
+    //     return +result;
+    // }else {
+    //     return result.toString();
+    // }
+    return result;
     }
     const combinedAges= combine(30, 26, 'as-number');
     console.log(combinedAges);
